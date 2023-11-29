1c9e86b148b4709b191de98272492b8eb3eaee17
Harsh <hgaur491@gmail.com>
2023-11-29 21:07:51 +0530

    fixing sidebar closs on clicking on anywere else other then button

--- src/components/pageLayouts/SideNavbar.jsx
+++ src/components/pageLayouts/SideNavbar.jsx
@@ -4,30 +4,38 @@ import { sideNavbarData } from "../../siteDB/SiteData";
 import { useContext} from "react";
 import Context_DB from "../../client_DB/Context_DB";
 import ThemeToggle from "../ThemeToggle";
-  
+import { Actions } from "../../client_DB/ReducerDB";
 export default function SideNavbar() {
-    const [state]=useContext(Context_DB);
-
+    const [state,dispatch]=useContext(Context_DB);
+	const handleToggle =()=>{
+		dispatch({
+			actionType: Actions.SIDEBAR_TOGGLE,
+			payload: !state.isSideBarOpen
+		})
+	}
     return (
-        <nav className={`
-             z-40
-            ${state.isSideBarOpen?"w-36":"w-0 sm:w-12"} hover:w-36
-            transition-width duration-200 ease-in-out overflow-hidden
-            fixed top-0 -bottom-14 pt-20 pb-20 pt- left-0 right-auto bg-bg_2/80 backdrop-blur-md
-            `}>
-            <ul className="p-2 capitalize container font-medium flex flex-col gap-2 h-full">
-                {sideNavbarData.navLinks.map(navLink =>(
-                    <li key={navLink.title} className={`${navLink?.linkClassName}`}>
-                        <Link className=" whitespace-nowrap flex gap-4 transition-colors duration-300 hover:text-accent p-1" to={`#${navLink.linkTo}`}>
-                            <div className=" w-8 h-6 text-2xl"><Icon icon={navLink.icon} /></div>
-                            <div className="w-full">{navLink.title}</div>
-                        </Link>
-                    </li>
-                ))}
-                    <li className={`mt-auto`}>
-                        <ThemeToggle/>
-                    </li>
-            </ul>
-        </nav>
+        <>
+            <nav className={`
+                z-40
+                ${state.isSideBarOpen?"w-36":"w-0 sm:w-12"} hover:w-36
+                transition-width duration-200 ease-in-out overflow-hidden
+                fixed top-0 -bottom-14 pt-20 pb-20 pt- left-0 right-auto bg-bg_2/80 backdrop-blur-md
+                `}>
+                <ul className="p-2 capitalize container font-medium flex flex-col gap-2 h-full">
+                    {sideNavbarData.navLinks.map(navLink =>(
+                        <li key={navLink.title} className={`${navLink?.linkClassName}`}>
+                            <Link className=" whitespace-nowrap flex gap-4 transition-colors duration-300 hover:text-accent p-1" to={`#${navLink.linkTo}`}>
+                                <div className=" w-8 h-6 text-2xl"><Icon icon={navLink.icon} /></div>
+                                <div className="w-full">{navLink.title}</div>
+                            </Link>
+                        </li>
+                    ))}
+                        <li className={`mt-auto`}>
+                            <ThemeToggle/>
+                        </li>
+                </ul>
+            </nav>
+            <div onClick={handleToggle} className={` ${state.isSideBarOpen?"fixed inset-0":""} bg-bg_1/50 z-30`}></div>
+        </>
 	);
 }
