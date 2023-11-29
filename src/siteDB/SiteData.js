13d78bc93394d03e9837aea14906e3d338527cda
Harsh <hgaur491@gmail.com>
2023-11-29 16:54:16 +0530

    completing contact ssection inside aboutme section

--- src/siteDB/SiteData.js
+++ src/siteDB/SiteData.js
@@ -48,6 +48,161 @@ const sideNavbarData = {
 		},
 	],
 };
+
+const aboutMeData = {
+	aboutMe: {
+		heading_1: "my name is harsh",
+		heading_2: "i am a web developer",
+		description:
+			"This is the first paragraph of the description of the web developer that will be available to you when you start developing your application and will be available to you when you start developing your application and will be available to you when you start developing ",
+		projectsCompleted: 5,
+		ongoingProjects: 5,
+	},
+	experience: [
+		{
+			date: "2023 to present",
+			jobRole: "front end developer",
+			companyName: "",
+			jobDescription:
+				"This is the description of your application and will be available to you when you start developing your application and will be available",
+		},
+		{
+			date: "2023 to present",
+			jobRole: "front end developer",
+			companyName: "",
+			jobDescription:
+				"This is the description of your application and will be available to you when you start developing your application and will be available",
+		},
+	],
+	skills: {
+		programmingSKills: [
+			{
+				skillName: "javascript",
+				skillLevel: "80%",
+				icon: "ion:logo-javascript",
+				color: "text-yellow-500", // Replace with your preferred color class
+			},
+			{
+				skillName: "react",
+				skillLevel: "70%",
+				icon: "ri:reactjs-line",
+				color: "text-blue-500", // Replace with your preferred color class
+			},
+			{
+				skillName: "tailwind",
+				skillLevel: "90%",
+				icon: "teenyicons:tailwind-outline",
+				color: "text-cyan-500", // Replace with your preferred color class
+			},
+			{
+				skillName: "express js",
+				skillLevel: "60%",
+				icon: "devicon:express",
+				color: "text-green-500", // Replace with your preferred color class
+			},
+			{
+				skillName: "node js",
+				skillLevel: "60%",
+				icon: "logos:nodejs",
+				color: "text-green-800", // Replace with your preferred color class
+			},
+			{
+				skillName: "rest-full apis",
+				skillLevel: "80%",
+				icon: "eos-icons:api-outlined",
+				color: "text-purple-500", // Replace with your preferred color class
+			},
+			{
+				skillName: "mongodb",
+				skillLevel: "50%",
+				icon: "devicon-plain:mongodb-wordmark",
+				color: "text-green-500", // Replace with your preferred color class
+			},
+			{
+				skillName: "git",
+				skillLevel: "70%",
+				icon: "raphael:github",
+				color: "text-orange-500", // Replace with your preferred color class
+			},
+			{
+				skillName: "github",
+				skillLevel: "90%",
+				icon: "mdi:github",
+				color: "text-gray-500", // Replace with your preferred color class
+			},
+			{
+				skillName: "html",
+				skillLevel: "96%",
+				icon: "nonicons:html-16",
+				color: "text-red-500", // Replace with your preferred color class
+			},
+			{
+				skillName: "css",
+				skillLevel: "80%",
+				icon: "bi:filetype-css",
+				color: "text-blue-500", // Replace with your preferred color class
+			},
+			{
+				skillName: "bootstrap",
+				skillLevel: "70%",
+				icon: "ri:bootstrap-line",
+				color: "text-purple-500", // Replace with your preferred color class
+			},
+		],
+		softSkills: [
+			{
+				skillName: "figma",
+				skillLevel: "80%",
+				icon: "",
+				color: "",
+			},
+		],
+	},
+	contacts: {
+		image: "la:dragon",
+		contacts: [
+			{
+				icon: "ph:phone",
+				color: "text-blue-500", // Replace with your preferred color class
+				contactType: "phone number",
+				linkTo:"tel:9310745921",
+			},
+			{
+				icon: "ic:baseline-whatsapp",
+				color: "text-green-500", // Replace with your preferred color class
+				contactType: "whatsapp",
+				linkTo:"https://wa.me/8588828394",
+				
+			},
+			{
+				icon: "basil:gmail-outline",
+				color: "text-red-500", // Replace with your preferred color class
+				contactType: "gmail",
+				linkTo:"mailto:hgaur491@gmail.com",
+				
+			},
+			{
+				icon: "mdi:linkedin",
+				color: "text-indigo-500", // Replace with your preferred color class
+				contactType: "linkedin",
+				linkTo:"https://www.linkedin.com/in/harshg775",
+			},
+			{
+				icon: "mdi:github",
+				color: "text-gray-500", // Replace with your preferred color class
+				contactType: "github",
+				linkTo:"https://github.com/harshG775",
+			},
+			{
+				icon: "ph:file-pdf",
+				color: "text-red-500", // Replace with your preferred color class
+				contactType: "resume",
+				linkTo:"/Harsh-_-resume_.pdf",
+                download:true,
+			},
+		],
+	},
+};
 const projectsData = [
     {
         preview:"https://tapsi-react.vercel.app/assets/img/project-3.jpg",
@@ -86,4 +241,4 @@ const projectsData = [
         description:"Lorem ipsum Lorem ipsum Lorem ipsum"
     },
 ]
-export { topNavbarData,sideNavbarData,logo,projectsData };
+export { topNavbarData,sideNavbarData,logo,  aboutMeData,projectsData };
